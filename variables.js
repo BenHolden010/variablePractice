77b4963a3987c4591c6f64a9ccec8b38da5fc3a0
Benjamin holden <benjamin.f.holden@outlook.com>
2023-03-01 00:48:30 -0700

    Add reassign values and console.log statements

--- variables.js
+++ variables.js
@@ -7,3 +7,15 @@ var papa = 64
 var hungry = true
 var thirsty = false
 var sleepy = true
+var email = "ben@gmail.com" 
+var name = "benny"
+var car = "chevy"
+var son = 10
+var father = 45
+var papa = 72
+var hungry = false
+var thirsty = true
+var sleepy = false
+console.log(son)
+console.log(hungry)
+console.log(car)
