a68566191d45e4e7c758b689f14187010737a1e9
Benjamin holden <benjamin.f.holden@outlook.com>
2023-03-01 11:36:29 -0700

    Remove var from reassignments

--- variables.js
+++ variables.js
@@ -7,15 +7,15 @@ var papa = 64
 var hungry = true
 var thirsty = false
 var sleepy = true
-var email = "ben@gmail.com" 
-var name = "benny"
-var car = "chevy"
-var son = 10
-var father = 45
-var papa = 72
-var hungry = false
-var thirsty = true
-var sleepy = false
+email = "ben@gmail.com" 
+name = "benny"
+car = "chevy"
+son = 10
+father = 45
+papa = 72
+hungry = false
+thirsty = true 
+sleepy = false
 console.log(son)
 console.log(hungry)
 console.log(car)
